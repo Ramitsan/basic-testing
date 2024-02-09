b81503d61615ed734fd742c1c7dd87f1995b4910
Ramitsan <ramitsan@gmail.com>
2024-02-10 01:13:03 +0300

    feat: implement tests for fs module from Mocking Node.js API

--- src/06-mocking-node-api/index.test.ts
+++ src/06-mocking-node-api/index.test.ts
@@ -1,5 +1,6 @@
 // Uncomment the code below and write your tests
-// import { readFileAsynchronously, doStuffByTimeout, doStuffByInterval } from '.';
+// import fs from "fs/promises";
+import { readFileAsynchronously, /* doStuffByTimeout, doStuffByInterval */ } from '.';
 
 describe('doStuffByTimeout', () => {
   beforeAll(() => {
@@ -37,16 +38,51 @@ describe('doStuffByInterval', () => {
   });
 });
 
+jest.mock("fs/promises", () => {
+  return {
+    readFile: async (path: string)=>{
+      expect(path).toBe('correctPath');
+      return "data";
+    }
+  };
+});
+
+jest.mock("fs", () => {
+  return {
+    existsSync: (path: string)=>{
+      return path === "correctPath";
+    }
+  };
+});
+
+let isJoinCalled = false;
+
+jest.mock("path", () => {
+  return {
+    join: (...paths: string[])=>{
+      isJoinCalled = true;
+      expect(typeof paths[1]).not.toBe('undefined');
+      return paths[1];
+    }
+  };
+});
+
 describe('readFileAsynchronously', () => {
   test('should call join with pathToFile', async () => {
     // Write your test here
+    await readFileAsynchronously('correctPath'); // returns promise
+    expect(isJoinCalled).toBe(true);
   });
 
   test('should return null if file does not exist', async () => {
     // Write your test here
+    const result = await readFileAsynchronously('wrongPath'); 
+    expect(result).toBe(null);
   });
 
   test('should return file content if file exists', async () => {
     // Write your test here
+    const result = await readFileAsynchronously('correctPath'); 
+    expect(result).toBe('data');
   });
 });

04606b867527d5d0aff76ada07583cd25bc41613
Ramitsan <ramitsan@gmail.com>
2024-02-10 05:09:34 +0300

    fix: empty methods from Partial mocking

--- src/05-partial-mocking/index.test.ts
+++ src/05-partial-mocking/index.test.ts
@@ -6,9 +6,9 @@ jest.mock('./index', () => {
     jest.requireActual<typeof import('./index')>('./index');
   return {
     ...originalModule,
-    mockOne: () => {},
-    mockTwo: () => {},
-    mockThree: () => {},
+    mockOne: jest.fn(),
+    mockTwo: jest.fn(),
+    mockThree: jest.fn(),
   };
 });
 
